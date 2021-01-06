f8504d8ac6dbdf514dbe2ce42877af0ffcf4d5d0
Steven Wadsworth <Stwadworth27@gmail.com>
2021-01-05 20:29:59 -0500

    Added ship to screen and settings class

--- Main.py
+++ Main.py
@@ -1,22 +1,31 @@
 import pygame
 import sys 
+from settings import Settings 
+from ship import Ship
 
 def run_game():
     pygame.init()
-    screen=pygame.display.set_mode((1200,800))
+    ai_settings=Settings()
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Set background color
-    bg_color=(230,230,230)
+    #make a ship
+    ship=Ship(screen)
 
 #main game loop
+    
     while True: 
         #Watch for keyboard and mouse events
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 sys.exit()
         #redraw the screen on each passthrough
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
         #most recent scren visible
         pygame.display.flip()
 run_game()
+<<<<<<< HEAD
+
+=======
+>>>>>>> parent of 8dd99e5 (Revert "Added ship image and starting position")
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    # A class to store settings for alien invasion
+
+    def __init__(self):
+        #initialize the games default settings
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color =(230,230,230)
